7d139f05a31f0212fa62fec79d6b1d55ebb4487e
jvanderzande <jvdzande@yahoo.com>
2019-01-15 10:56:16 +0100

    Added script_time_opzet.lua: This script is a generic script for all gemeentes that use the service of opzet.nl. This makes the separate scripts for Pumerend, Zuidwest Friesland and mijnblink (for the gemeentes: Laarbeek, Deurne, Gemert-Bakel, Heeze-Leende, Someren, Asten en Nuenen) obsolete as they are supported by this script. check their website for supported gemeentes: http://www.opzet.nl/afvalkalender_digitaal or check the afvalkalender of your gemeente and check whether it refers at the bottom of the page to "Ontwerp & techniek: Opzet".

--- README.md
+++ README.md
@@ -7,17 +7,22 @@ This version is based on the idea from this thread/posts:
 by zicht @ http://www.domoticz.com/forum/viewtopic.php?t=17963
 script version by nf999 @ http://www.domoticz.com/forum/viewtopic.php?f=61&t=17963&p=174908#p169637
 
+# huisvuil-opzet
+This script is a generic script for all gemeentes that use the service of opzet.nl.
+This makes the separate scripts for Pumerend, Zuidwest Friesland and mijnblink (for the gemeentes: Laarbeek, Deurne, Gemert-Bakel, Heeze-Leende, Someren, Asten en Nuenen) obsolete as they are supported by this script. check their website for supported gemeentes: http://www.opzet.nl/afvalkalender_digitaal
+or check the afvalkalender of your gemeente and check whether it refers at the bottom of the page to "Ontwerp & techniek: Opzet".
+
+# huisvuil-purmerend
+(Please use script_time_huisvuil_opzet.lua going forward)
+
+# huisvuil-zuidwest-friesland
+(Please use script_time_huisvuil_opzet.lua going forward)
+
+# huisvuil-mijnblink
+(Please use script_time_huisvuil_opzet.lua going forward)
+
 # huisvuil-westland
 This script is for the site huisvuilkalender.gemeentewestland.nl for the gemeente Westland.
 
-# huisvuil-purmerend
-This script is for the site afvalkalender.purmerend.nl for the gemeente Purmerend & Beemster.
-
-# huisvuil-zuidwest-friesland
-This script is for the site afvalkalender.sudwestfryslan.nl for the gemeentes in Zuidwest Friesland.
-
 # huisvuil-zuidlimburg (RD4)
 This script is for the site www.rd4info.nl for the gemeentes in Zuid Limburg.
-
-# huisvuil-mijnblink
-This script is for the site mijnblink.nl for the gemeentes: Laarbeek, Deurne, Gemert-Bakel, Heeze-Leende, Someren, Asten en Nuenen.
---
+++ script_time_huisvuil-opzet.lua
@@ -0,0 +1,225 @@
+-----------------------------------------------------------------------------------------------------------------
+-- huisvuilkalender script: script_time_opzet.lua used for gemeentes using  http://www.opzet.nl/afvalkalender_digitaal
+----------------------------------------------------------------------------------------------------------------
+ver="20190115-1100"
+-- curl in os required!!
+-- create dummy text device from dummy hardware with the name defined for: myAfvalDevice
+-- Check the timing when to get a notification for each Afvaltype in the afvaltype_cfg table
+-- Check forumtopic:       https://www.domoticz.com/forum/viewtopic.php?f=61&t=17963
+-- Check source updates:   https://github.com/jvanderzande/mijnafvalwijzer
+-- Link to WebSite:        http://Hostname/rest/adressen/.....
+--
+myAfvalDevice = 'Container'      -- The Text devicename in Domoticz
+hostname = ""                    -- Specify the hostname of your afvalwebsite. eg: "afvalkalender.purmerend.nl", "afvalkalender.sudwestfryslan.nl", "mijnblink.nl" ..etc
+ShowNextEvents = 3               -- indicate the next x events to show in the TEXT Sensor in Domoticz
+Postcode = ''                    -- Postcode
+Housenr = ''                     -- Huisnummer zonder toevoeging
+Housenrtoev=''                   -- Huisnummer toevoeging
+NotificationEmailAdress = ""     -- Specify your Email Address for the notifications. Leave empty to skip email notification
+Notificationsystem = ""          -- Specify notification system eg "telegram/pushover/.." leave empty to skip
+
+-- Switch on Debugging in case of issues => set to true/false=======
+local debug = false  -- get debug info in domoticz console/log
+
+-- define a line for each afvaltype_cfg retuned by the webrequest:
+   -- hour & min ==> the time the check needs to be performed and notification send when daysbefore is true
+   -- daysbefore ==> 0 means that the notification is send on the day of the planned garbage collection
+   -- daysbefore ==> X means that the notification is send X day(s) before the day of the planned garbage collection
+   -- reminder   ==> Will send a second reminder after x hours. 0=no reminder (needs to be in the same day!)
+   -- text       ==> define the text for the notification.
+local afvaltype_cfg = {
+   ["Restafval"]                        ={hour=19,min=02,daysbefore=1,reminder=0,text="Container met Restafval"},
+   ["GFT"]                              ={hour=19,min=02,daysbefore=1,reminder=0,text="Container met Tuinfval"},
+   ["Plastic, Metaal en Drankkartons"]  ={hour=19,min=02,daysbefore=1,reminder=0,text="PMD bak"},
+   ["Papier en karton"]                 ={hour=12,min=01,daysbefore=0,reminder=0,text="Oud papier"},
+   ["Kerstbomen"]                       ={hour=19,min=22,daysbefore=1,reminder=0,text="Kerstbomen"},
+   ["Dummy1"]                           ={hour=02,min=02,daysbefore=0,reminder=0,text="dummy"},   -- dummy is used to force update while testing
+   ["Dummy2"]                           ={hour=02,min=02,daysbefore=0,reminder=0,text="dummy"}}   -- dummy is used to update the textsensor at night for that day
+--==== end of config ======================================================================================================
+
+-- General conversion tables
+local MON={jan=1,feb=2,maa=3,apr=4,mei=5,jun=6,jul=7,aug=8,sep=9,okt=10,nov=11,dec=12}
+
+-- round
+function Round(num, idp)
+   return tonumber(string.format("%." ..(idp or 0).. "f", num))
+end
+-- debug print
+function dprint(text)
+   if debug then print("@AFOpzet::"..text) end
+end
+-- run curl and capture output
+function os.capture(cmd, rep)  -- execute command to get site
+   -- rep is nr of repeats if result is empty
+   local r = rep or 1
+   local s = ""
+   while ( s == "" and r > 0) do
+      r = r-1
+      local f = assert(io.popen(cmd, 'r'))
+      s = assert(f:read('*a'))
+      f:close()
+   end
+   if ( rep - r > 1 ) then
+      print("os.capture needed more than 1 call: " .. rep-r)
+   end
+  return s
+end
+-- get days between today and provided date
+function getdaysdiff(i_afvaltype_date)
+   local curTime = os.time{day=timenow.day,month=timenow.month,year=timenow.year}
+   -- Calculate the daysdifference between found date and Now and send notification is required
+   local afvalyear =timenow.year
+   local afvalday  =timenow.day
+   local afvalmonth=timenow.month
+   local s_afvalmonth="vandaag"
+   -- check if date in variable i_afvaltype_date contains "vandaag" in stead of a valid date -> use today's date
+   if i_afvaltype_date == "vandaag" then
+      -- use the set todays info
+   else
+      --s_afvalmonth, afvalday=i_afvaltype_date:match("(%a-). (%d+), %d+")
+      afvalday, s_afvalmonth=i_afvaltype_date:match("%a- (%d-) (%a+)$")
+      if (afvalday == nil or s_afvalmonth == nil) then
+         print ('@AFOpzet Error: No valid date found in i_afvaltype_date: ' .. i_afvaltype_date)
+         return
+      end
+      afvalmonth = MON[s_afvalmonth]
+   end
+   dprint("...gerd-> afvalyear:"..tostring(afvalyear).."  s_afvalmonth:"..tostring(s_afvalmonth).."  afvalmonth:"..tostring(afvalmonth).."  afvalday:"..tostring(afvalday))
+   --
+   local afvalTime = os.time{day=afvalday,month=afvalmonth,year=afvalyear}
+   -- return number of days diff
+   return Round(os.difftime(afvalTime, curTime)/86400,0)   -- 1 day = 86400 seconds
+end
+
+function notification(s_afvaltype,s_afvaltype_date,i_daysdifference)
+   dprint("...Noti-> i_daysdifference:"..tostring(i_daysdifference).."  afvaltype_cfg[s_afvaltype].daysbefore:"..tostring(afvaltype_cfg[s_afvaltype].daysbefore).."  hour:"..tostring(afvaltype_cfg[s_afvaltype].hour).."  min:"..tostring(afvaltype_cfg[s_afvaltype].min))
+   if afvaltype_cfg[s_afvaltype] ~= nil
+   and (timenow.hour==afvaltype_cfg[s_afvaltype].hour or timenow.hour==afvaltype_cfg[s_afvaltype].hour+afvaltype_cfg[s_afvaltype].reminder)
+   and timenow.min==afvaltype_cfg[s_afvaltype].min
+   and i_daysdifference == afvaltype_cfg[s_afvaltype].daysbefore then
+      dag = ""
+      if afvaltype_cfg[s_afvaltype].daysbefore == 0 then
+         dag = "vandaag"
+      elseif afvaltype_cfg[s_afvaltype].daysbefore == 1 then
+         dag = "morgen"
+      else
+         dag = "over " .. tostring(afvaltype_cfg[s_afvaltype].daysbefore) .. " dagen"
+      end
+      notificationtitle = notificationtitle:gsub('@DAG@',dag)
+      notificationtitle = notificationtitle:gsub('@AFVALTYPE@',s_afvaltype)
+      notificationtitle = notificationtitle:gsub('@AFVALTEXT@',tostring(afvaltype_cfg[s_afvaltype].text))
+      notificationtitle = notificationtitle:gsub('@AFVALDATE@',s_afvaltype_date)
+      notificationtext = notificationtext:gsub('@DAG@',dag)
+      notificationtext = notificationtext:gsub('@AFVALTYPE@',s_afvaltype)
+      notificationtext = notificationtext:gsub('@AFVALTEXT@',tostring(afvaltype_cfg[s_afvaltype].text))
+      notificationtext = notificationtext:gsub('@AFVALDATE@',s_afvaltype_date)
+      if NotificationEmailAdress ~= "" then
+         commandArray['SendEmail'] = notificationtitle .. '#' .. notificationtext .. '#' .. NotificationEmailAdress
+         dprint ('Notification Email send for ' .. s_afvaltype.. " |"..notificationtitle .. '#' .. notificationtext .. '#' .. NotificationEmailAdress.."|")
+      end
+      if Notificationsystem ~= "" then
+         commandArray['SendNotification']=notificationtitle .. '#' .. notificationtext .. '#' .. NotificationEmailAdress.."###"..Notificationsystem
+         dprint ('Notification '..Notificationsystem..' send for '.. s_afvaltype.. " |"..notificationtitle .. '#' .. notificationtext .. '#' .. NotificationEmailAdress.."|")
+      end
+      dprint ('AFOpzet Notification send for ' .. s_afvaltype.. "  title:|"..notificationtitle.. "|  body:|"..notificationtext.."|")
+   end
+end
+
+-- Do the actual update retrieving data from the website and processing it
+function Perform_Update()
+   print('@AFOpzet module start check for '..Postcode.."-"..Housenr)
+   dprint('=== web update ================================')
+   -- get data from the website
+   local commando = "curl --max-time 5 -s \"https://"..hostname.."/adres/"..Postcode..":"..Housenr..":"..Housenrtoev.."\""
+   dprint(commando)
+   local tmp = os.capture(commando, 1)
+   if ( tmp == "" ) then
+      print("@AFOpzet Error: Empty result from curl command, skipping run.")
+      return
+   else
+      --dprint("website data tmp="..tmp)
+   end
+   -- Retrieve part with the dates for pickup
+   tmp=tmp:match('.<ul id="ophaaldata" class="line">(.-)<footer>')
+   if tmp == nil or tmp == '' then
+      print ('@AFOpzet Error: Could not find the ophaaldata section in the data.  skipping the rest of the logic.')
+      return
+   end
+   dprint("- start looping through received data -----------------------------------------------------------")
+   local web_afvaltype = ""
+   local web_afvaldate = ""
+   local missingrecords = ""
+   local txt = ""
+   local cnt = 0
+
+--   Loop through all dates
+   for web_afvaltype, web_afvaldate in string.gmatch(tmp, 'title="Naar afvalstroom (.-)">.-class="date">(.-)</i>') do
+      if web_afvaltype~= nil and web_afvaldate ~= nil then
+         -- first match for each Type we save the date to capture the first next dates
+         dprint(web_afvaltype,web_afvaldate)
+         daysdiffdev = getdaysdiff(web_afvaldate)
+         -- When days is 0 or greater the date is today or in the future. Ignore any date in the past
+         if daysdiffdev >= 0 then
+            -- fill the text with the next defined number of events
+            if cnt < ShowNextEvents then
+               txt = txt..web_afvaldate .. "=" .. web_afvaltype .. "\r\n"
+               cnt=cnt+1
+            end
+         end
+         if afvaltype_cfg[web_afvaltype] ~= nil and afvaltype_cfg[web_afvaltype].daysbefore ~= nil then
+            -- check if notification needs to be send
+            notification(web_afvaltype,web_afvaldate,daysdiffdev)
+         else
+            print ('@AFOpzet Error: Afvalsoort not defined in the "afvaltype_cfg" table for found Afvalsoort : ' .. web_afvaltype)
+            missingrecords = missingrecords .. '   ["' .. web_afvaltype..'"]'..string.rep(" ", 32-string.len(web_afvaltype))..' ={hour=19,min=22,daysbefore=1,reminder=0,text="'..web_afvaltype..'"},\n'
+         end
+      end
+   end
+   dprint("-End   --------------------------------------------------------------------------------------------")
+   if missingrecords ~= "" then
+      print('#### -- start -- Add these records to local afvaltype_cfg = {')
+      print(missingrecords)
+      print('#### -- end ----------------------------')
+   end
+   if (cnt==0) then
+      print ('@AFOpzet Error: No valid data found in returned webdata.  skipping the rest of the logic.')
+      return
+   end
+   -- always update the domoticz device so one can see it is updating and when it was ran last.
+   print ('@AFOpzet Found: '..txt:gsub('\r\n', ' ; '))
+   if otherdevices_idx == nil or otherdevices_idx[myAfvalDevice] == nil then
+      print ("@AFOpzet Error: Couldn't get the current data from Domoticz text device "..myAfvalDevice )
+   else
+      commandArray['UpdateDevice'] = otherdevices_idx[myAfvalDevice] .. '|0|' .. txt
+      if (otherdevices[myAfvalDevice] ~= txt) then
+         print ('@AFOpzet: Update device from: \n'.. otherdevices[myAfvalDevice] .. '\n replace with:\n' .. txt)
+      else
+         print ('@AFOpzet: No updated text for TxtDevice.')
+      end
+   end
+end
+
+-- End Functions =========================================================================
+
+-- Start of logic ========================================================================
+commandArray = {}
+timenow = os.date("*t")
+
+-- check for notification times and run update only when we are at one of these defined times
+local needupdate = false
+for avtype,get in pairs(afvaltype_cfg) do
+   dprint("afvaltype_cfg :"..tostring(avtype)..";"..tostring(afvaltype_cfg[avtype].hour)..";"..tostring(afvaltype_cfg[avtype].min))
+   if (timenow.hour==afvaltype_cfg[avtype].hour
+   or  timenow.hour==afvaltype_cfg[avtype].hour+afvaltype_cfg[avtype].reminder)
+   and timenow.min==afvaltype_cfg[avtype].min then
+      needupdate = true
+   end
+end
+-- get information from website, update device and send notification when required
+if needupdate then
+   Perform_Update()
+else
+   dprint("Scheduled time(s) not reached yet, so nothing to do!")
+end
+
+return commandArray
